b8b559405f1655476ae76fb6677ae8ba99d65af4
Junyoung Heo <jyheo0@gmail.com>
2015-11-03 16:11:52 +0000

    .

--- servo.py
+++ servo.py
@@ -5,11 +5,11 @@ import sys
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
-pwm = GPIO.PWM(18, 50)
+pwm = GPIO.PWM(18, 50) # 50Hz
 pwm.start(1)
 
 if len(sys.argv) != 2:
-	print 'sudo python servo.py (1~2)'
+	print 'sudo python servo.py 1~3'
 else:
 	ms = float(sys.argv[1])
 	dcycle = ms / 20.0 * 100
