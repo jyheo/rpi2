c692f6510b9d0ba9b44c12dfaf33cca307c5b95a
Junyoung Heo <jyheo0@gmail.com>
2015-11-15 18:33:54 +0000

    change framework to Flask

--- yl40-web.py
+++ yl40-web.py
@@ -1,31 +1,27 @@
 import smbus
 import time
 import sys
-import web
+from flask import Flask
+
+app = Flask(__name__)
 
 bus = smbus.SMBus(1)
 address = 0x48
 
-urls = ('/', 'index')
+@app.route("/ain/<int:ain_addr>")
+def analog_in(ain_addr):
+	# 192.168.0.6:5000/ain/0
+	bus.write_byte(address, 0x40 + ain_addr)
+	bus.read_byte(address)
+	aread = bus.read_byte(address)
+	return 'Ain%d %d' % (ain_addr, aread)
 
-class index:
-	def GET(self):
-		# 192.168.0.6:8080/?ain=0&aout=on&aout_val=255
-		params = web.input(ain=0, aout="off", aout_val=0)
-		print params
-		ain = int(params.ain)
-		aout = str(params.aout)
-		aout_val = int(params.aout_val)
-
-		bus.write_byte(address, 0x40 + ain)
-		bus.read_byte(address)
-		aread = bus.read_byte(address)
-
-		if aout == "on":
-			bus.write_byte_data(address, 0x40, aout_val)
-		return aread
+@app.route("/aout/<int:val>")
+def analog_out(val):
+	# 192.168.0.6:5000/aout/0
+	bus.write_byte_data(address, 0x40, val)
+	return 'Aout %d' % (val)
 
 if __name__ == "__main__":
-	app = web.application(urls, globals())
-	app.run()
+	app.run(host="0.0.0.0")
 
