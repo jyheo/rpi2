1725ec273db6f270adc027c1562555c53f3fcc21
Junyoung Heo <jyheo0@gmail.com>
2015-11-27 06:14:03 +0000

    ioctl example

---
+++ kmod/ioctl_ex.h
@@ -0,0 +1,15 @@
+#ifndef IOCTL_EX_H
+#define IOCTL_EX_H
+#include <linux/ioctl.h>
+
+typedef struct
+{
+	int status, dignity, ego;
+} query_arg_t;
+
+#define QUERY_GET_VARIABLES _IOR('q', 1, query_arg_t *)
+#define QUERY_CLR_VARIABLES _IO('q', 2)
+#define QUERY_SET_VARIABLES _IOW('q', 3, query_arg_t *)
+
+#endif
+
