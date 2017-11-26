27a02d0dbd18569b684ba5b1b6401ea6a4726af0
Junyoung Heo <jyheo0@gmail.com>
2017-11-26 16:38:17 +0000

    python3

--- servo.py
+++ servo.py
@@ -9,10 +9,13 @@ pwm = GPIO.PWM(18, 50) # 50Hz
 pwm.start(1)
 
 if len(sys.argv) != 2:
-	print 'sudo python servo.py 1~3'
+	print('sudo python servo.py 1~3')
 else:
 	ms = float(sys.argv[1])
 	dcycle = ms / 20.0 * 100
 	pwm.ChangeDutyCycle(dcycle)
 
 time.sleep(2)
+
+pwm.stop()
+GPIO.cleanup()
